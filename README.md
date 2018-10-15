eb9f32a7a43fc6aae4c6c75ec2f85e7608185a50
Ice-Hazymoon <imiku.me@gmail.com>
2018-10-15 19:25:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gbot
+
+基于酷Q制作的 GARNiDELiA 应援群推送机器人，用于订阅twitter和instagram的更新并转发到QQ群，尚未完善，开发中
---
+++ index.js
@@ -0,0 +1,134 @@
+"use strict";
+const CQHttp = require('cqhttp');
+const credentials = require('./credentials');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const MD5 = require('js-md5');
+const Agent = require('socks5-https-client/lib/Agent');
+const fs = require('fs').promises;
+
+let bot = new CQHttp({
+    apiRoot: 'http://127.0.0.1:5700/',
+    accessToken: credentials.accessToken,
+    secret: credentials.secret
+});
+
+function htmlDecode(str) {
+    // 一般可以先转换为标准 unicode 格式（有需要就添加：当返回的数据呈现太多\\\u 之类的时）
+    str = unescape(str.replace(/\\u/g, "%u"));
+    // 再对实体符进行转义
+    // 有 x 则表示是16进制，$1 就是匹配是否有 x，$2 就是匹配出的第二个括号捕获到的内容，将 $2 以对应进制表示转换
+    str = str.replace(/&#(x)?(\w+);/g, function ($, $1, $2) {
+        return String.fromCharCode(parseInt($2, $1 ? 16 : 10));
+    });
+    return str;
+}
+
+async function fanyi(str) {
+    const appid = credentials.baidu.appid;
+    const key = credentials.baidu.key;
+    const salt = (new Date).getTime();
+    const query = str;
+    const from = 'jp';
+    const to = 'zh';
+    const str1 = appid + query + salt + key;
+    const sign = MD5(str1);
+
+    let data = await rp.get('http://api.fanyi.baidu.com/api/trans/vip/translate', {
+        qs: {
+            q: query,
+            appid: appid,
+            salt: salt,
+            from: from,
+            to: to,
+            sign: sign
+        }
+    });
+    return JSON.parse(data);
+}
+
+async function h(_name, _rssUrl, _title, _rss_version) {
+    let _RSS_VERSION = _rss_version;
+    try {
+        let e = await rp(_rssUrl, {
+            json: true
+        })
+        let date = (new Date(e.items[0].date_published)).getTime();
+        if (_RSS_VERSION === null) {
+            console.log('首次请求' + _name);
+            _RSS_VERSION = date;
+            setTimeout(() =>{
+                h(_name, _rssUrl, _title, _RSS_VERSION);
+            }, 1000 * 60 * 1)
+            return false;
+        }
+        if (_RSS_VERSION < date) {
+            console.log(_name + '有更新');
+            const $ = cheerio.load('<div class="_x">' + htmlDecode(e.items[0].summary) + '</div>');
+            let imgArr = '';
+            for (let index = 0; index < $('img').length; index++) {
+                const element = $('img')[index];
+                let src = $(element).attr('src');
+                async function _(){
+                    if (src) {
+                        try {
+                            let response = await rp({
+                                method: 'GET',
+                                url: src,
+                                agentClass: Agent,
+                                agentOptions: {
+                                    socksHost: '127.0.0.1',
+                                    socksPort: 1080
+                                },
+                                encoding: null
+                            })
+                            const data = "base64://" + Buffer.from(response, 'utf-8').toString('base64');
+                            imgArr += '[CQ:image,file=' + data + ']';
+                        } catch (error) {
+                            console.log(_name + '图片抓取失败\n' + error);
+                        }
+                    };
+                }
+                await _();
+            }
+
+            let text = $('._x').text();
+            let fanyiText = await fanyi(text);
+            let img = imgArr === '' ? '暂无' : imgArr;
+            bot('send_group_msg', {
+                group_id: 57556801,
+                message: _title +
+                '\n-------------------------------------------' +
+                '\n内容：' + text +
+                '\n翻译：' + fanyiText.trans_result[0].dst +
+                '\n媒体：\n' + img +
+                '\n-------------------------------------------' +
+                '\n原链接：' + e.items[0].url +
+                '\n日期：' + e.items[0].date_published +
+                '\n来自————月月的机器人' +
+                '\n问题反馈群内联系：1733708055'
+            }).then(() => {
+                _RSS_VERSION = date;
+                console.log(_name + '更新发送成功');
+            }).catch(err => {
+                console.log(_name + '更新发送失败\n' + err);
+            })
+        }
+        setTimeout(() => {
+            h(_name, _rssUrl, _title, _RSS_VERSION);
+        }, 1000 * 60 * 5);
+    } catch (error) {
+        console.log('请求RSSHub失败\n' + error);
+        setTimeout(() => {
+            h(_name, _rssUrl, _title, _RSS_VERSION);
+        }, 1000 * 5);
+    }
+};
+h('Twitter_GARNIDELIA', 'https://rsshub.app/twitter/user/GARNiDELiA.json?limit=1', '【@GARNIDELIA】的Twitter更新了！！', null);
+h('Twitter_toku_grnd', 'https://rsshub.app/twitter/user/toku_grnd.json?limit=1', '【@toku_grnd】的Twitter更新了！！', null);
+h('Twitter_MARiA_GRND', 'https://rsshub.app/twitter/user/MARiA_GRND.json?limit=1', '【@MARiA_GRND】的Twitter更新了！！', null);
+
+h('Instagram_maria_grnd', 'https://rsshub.app/instagram/user/maria_grnd.json?limit=1', '【@maria_grnd】的Instagram更新了！！', null);
+h('Instagram_toku_grnd', 'https://rsshub.app/instagram/user/toku_grnd.json?limit=1', '【@toku_grnd】的Instagram更新了！！', null);
+
+bot.listen(8989, '127.0.0.1');
