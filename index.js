82a865e3dccbe1eefe1ef5e00ad0eca7205ef8b6
Ice-Hazymoon <imiku.me@gmail.com>
2018-10-18 10:30:38 +0800

    写了些什么jb

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function fanyi(str) {
     const key = credentials.baidu.key;
     const salt = (new Date).getTime();
     const query = str;
-    const from = 'jp';
+    const from = 'auto';
     const to = 'zh';
     const str1 = appid + query + salt + key;
     const sign = MD5(str1);
@@ -48,101 +48,105 @@ async function fanyi(str) {
     return JSON.parse(data);
 }
 
-async function h(_name, _rssUrl, _title, _rss_version) {
-    let _RSS_VERSION = _rss_version;
-    try {
-        let e = await rp(_rssUrl, {
-            json: true
-        })
-        let date = (new Date(e.items[0].date_published)).getTime();
-        if (_RSS_VERSION === null) {
-            console.log(clc.cyan(new Date() + '：') + '首次请求 ' + clc.magenta(_name));
-            _RSS_VERSION = date;
-            setTimeout(() =>{
-                h(_name, _rssUrl, _title, _RSS_VERSION);
-            }, 1000 * 60 * 1)
-            return false;
-        }
-        if (_RSS_VERSION < date) {
-            console.log(clc.cyan(new Date() + '：') + '发现更新 ' + clc.magenta(_name));
-            const $ = cheerio.load('<div class="_x">' + htmlDecode(e.items[0].summary) + '</div>');
-            let imgArr = '';
-            for (let index = 0; index < $('img').length; index++) {
-                const element = $('img')[index];
-                let src = $(element).attr('src');
-                async function _(){
-                    if (src) {
-                        try {
-                            let response = await rp({
-                                method: 'GET',
-                                url: src,
-                                agentClass: Agent,
-                                agentOptions: {
-                                    socksHost: '127.0.0.1',
-                                    socksPort: 1080
-                                },
-                                encoding: null
-                            })
+let v = new Object(); // 保存rss每次拉取的时间
+let baseURL = 'https://rsshub.app';
+function h(config, timeout) {
+    if (!timeout) timeout = 1000 * 60 * 5;
+    if (!v[config.name]) timeout = 0;
+    setTimeout(() => {
+        rp.get(baseURL + config.url, {
+            json: true,
+            qs: {
+                limit: 1
+            }
+        }).then(async e => {
+            const date_published = (new Date(e.items[0].date_published)).getTime();
+            if (!v[config.name]) { // 如果不存在说明是第一次请求
+                console.log(clc.cyan(new Date() + '：') + '首次请求 ' + clc.magenta(config.name));
+                v[config.name] = date_published;
+                h(config);
+                return false;
+            }
+            if (v[config.name] < date_published) { //有更新
+                console.log(clc.cyan(new Date() + '：') + '发现更新 ' + clc.magenta(config.name));
+                const $ = cheerio.load('<div class="_x">' + htmlDecode(e.items[0].summary) + '</div>');
+                let imgArr = '';
+                if ($('img').length !== 0){
+                    let promises = new Array();
+                    $('img').each(function (item) {
+                        const src = $(this).attr('src');
+                        promises.push(rp({
+                            method: 'GET',
+                            url: src,
+                            agentClass: Agent,
+                            agentOptions: {
+                                socksHost: '127.0.0.1',
+                                socksPort: 1080
+                            },
+                            encoding: null
+                        }))
+                    })
+                    try {
+                        let miao = await Promise.all(promises);
+                        miao.forEach(response => {
                             const data = "base64://" + Buffer.from(response, 'utf-8').toString('base64');
                             imgArr += '[CQ:image,file=' + data + ']';
-                        } catch (error) {
-                            console.log(clc.cyan(new Date() + '：') + clc.magenta(_name) + '：图片抓取失败\n' + clc.blackBright(error));
-                        }
-                    };
+                        });
+                    } catch (error) {
+                        console.log(clc.cyan(new Date() + '：') + clc.magenta(config.name) + '：图片抓取失败\n' + clc.blackBright(error));
+                        return false;
+                    }
                 }
-                await _();
+                const text = '【@' + config.name.split('-')[1] + '】的' + config.name.split('-')[0] + '更新了！';
+                const title = e.items[0].title === '' ? '' : ('\n标题：' + e.items[0].title);
+                const content = $('._x').text();
+                const fanyiText = (await fanyi(content)).trans_result[0].dst;
+                const imgs = imgArr === '' ? '暂无' : imgArr;
+                const url = e.items[0].url;
+                bot('send_group_msg', {
+                    group_id: 57556801,
+                    message: text +
+                        '\n-------------------------------------------' +
+                        title +
+                        '\n内容：' + content +
+                        '\n翻译：' + fanyiText +
+                        '\n媒体：\n' + imgs +
+                        '\n-------------------------------------------' +
+                        '\n原链接：' + url +
+                        '\n日期：' + date_published +
+                        '\n来自————月月的机器人' +
+                        '\n问题反馈群内联系：[CQ:at,qq=1733708055]'
+                }).then(() => {
+                    console.log(clc.cyan(new Date() + '：') + clc.magenta(config.name) + ' 更新发送成功');
+                    v[config.name] = date_published;
+                    h(config);
+                }).catch(err => {
+                    console.log(clc.cyan(new Date() + '：') + '更新发送失败\n' + clc.blackBright(err));
+                    h(config, 1000 * 60 * 1);
+                })
+            } else { //没有更新
+                console.log(clc.cyan(new Date() + '：') + clc.magenta(config.name) + ' 没有更新  最后更新于：' + clc.cyan(new Date(e.items[0].date_published)));
+                h(config);
             }
-
-            let text = $('._x').text();
-            let fanyiText = await fanyi(text);
-            let img = imgArr === '' ? '暂无' : imgArr;
-            let title = e.items[0].title === '' ? '' : ('\n标题：' + e.items[0].title);
-            bot('send_group_msg', {
-                group_id: 57556801,
-                message: _title +
-                '\n-------------------------------------------' +
-                title +
-                '\n内容：' + text +
-                '\n翻译：' + fanyiText.trans_result[0].dst +
-                '\n媒体：\n' + img +
-                '\n-------------------------------------------' +
-                '\n原链接：' + e.items[0].url +
-                '\n日期：' + e.items[0].date_published +
-                '\n来自————月月的机器人' +
-                '\n问题反馈群内联系：1733708055'
-            }).then(() => {
-                _RSS_VERSION = date;
-                console.log(clc.cyan(new Date() + '：') + clc.magenta(_name) + ' 更新发送成功');
-            }).catch(err => {
-                console.log(clc.cyan(new Date() + '：') + '更新发送失败\n' + clc.blackBright(err));
-            })
-        } else {
-            console.log(clc.cyan(new Date() + '：') + clc.magenta(_name) + ' 没有更新，最后更新于：' + clc.cyan(new Date(e.items[0].date_published) + '：'));
-        }
-        setTimeout(() => {
-            h(_name, _rssUrl, _title, _RSS_VERSION);
-        }, 1000 * 60 * 5);
-    } catch (error) {
-        console.log(clc.cyan(new Date() + '：') + clc.magenta(_name) + '：请求RSSHub失败\n' + clc.blackBright(error));
-        setTimeout(() => {
-            h(_name, _rssUrl, _title, _RSS_VERSION);
-        }, 1000 * 5);
-    }
+        }).catch(error => {
+            console.log(clc.cyan(new Date() + '：') + clc.magenta(config.name) + '  请求RSSHub失败\n' + clc.blackBright(error));
+            h(config, 1000 * 60 * 1);
+        })
+    }, timeout);
 };
 
-// Twitter
-h('Twitter_GARNIDELIA', 'https://rsshub.app/twitter/user/GARNiDELiA.json?limit=1', '【@GARNIDELIA】的Twitter更新了！！', null);
-h('Twitter_toku_grnd', 'https://rsshub.app/twitter/user/toku_grnd.json?limit=1', '【@toku_grnd】的Twitter更新了！！', null);
-h('Twitter_MARiA_GRND', 'https://rsshub.app/twitter/user/MARiA_GRND.json?limit=1', '【@MARiA_GRND】的Twitter更新了！！', null);
-
-// Weibo
-h('Weibo_MARiA_GARNiDELiA', 'https://rsshub.app/weibo/user2/2060888642.json?limit=1', '【@MARiA_GARNiDELiA】的Weibo更新了！！', null);
-
-// Bilibili
-h('Bilibili_GARNiDELiA', 'https://rsshub.app/bilibili/user/dynamic/111939335.json?limit=1', '【@GARNiDELiA】的Bilibili更新了！！', null);
-
-// Instagram
-h('Instagram_maria_grnd', 'https://rsshub.app/instagram/user/maria_grnd.json?limit=1', '【@maria_grnd】的Instagram更新了！！', null);
-h('Instagram_toku_grnd', 'https://rsshub.app/instagram/user/toku_grnd.json?limit=1', '【@toku_grnd】的Instagram更新了！！', null);
+// // Twitter
+h({ name: 'Twitter-GARNIDELIA', url: '/twitter/user/GARNiDELiA.json'});
+h({ name: 'Twitter-toku_grnd', url: '/twitter/user/toku_grnd.json'});
+h({ name: 'Twitter-MARiA_GRND', url: '/twitter/user/MARiA_GRND.json'});
+// // Weibo
+h({ name: 'Weibo-MARiA_GARNiDELiA', url: '/weibo/user2/2060888642.json'});
+// // Bilibili
+h({ name: 'Bilibili-GARNiDELiA', url: '/bilibili/user/dynamic/111939335.json'});
+// // Instagram
+h({ name: 'Instagram-maria_grnd', url: '/instagram/user/maria_grnd.json'});
+h({ name: 'Instagram-toku_grnd', url: '/instagram/user/toku_grnd.json'});
+// // YouTube
+h({ name: 'YouTube-HeadphoneTokyo', url: '/youtube/user/HeadphoneTokyo.json'});
 
 bot.listen(8989, '127.0.0.1');
