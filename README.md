aef40a41faedb40330ea9822868e71d6d72dfff9
Hazymoon <imiku.me@gmail.com>
2018-12-25 22:18:13 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # Gbot
 
-基于酷Q制作的RSSHub QQ群推送机器人，用于订阅RSSHub的更新并转发到QQ群
+基于酷Q制作的 RSSHub QQ群推送机器人，用于订阅RSSHub的更新并转发到QQ群
 
-参考：[https://imiku.me/2018/10/16/1230.html](https://imiku.me/2018/10/16/1230.html)
+## 使用
+
+在根目录新建一个 `credentials.js` 文件，内容为
+
+```javascript
+module.exports = {
+    accessToken: '123456', //cqhttp插件配置
+    secret: '123456',
+    baidu: { //百度翻译的信息
+        appid: '123456',
+        key: '123456'
+    },
+    list: [
+        { name: 'Twitter-Ice_Hayzmoon', url: '/twitter/user/Ice_Hayzmoon', group_id: 57556801}, //订阅信息
+    ]
+}
+```
+
+## 其他
+
+[https://imiku.me/2018/10/16/1230.html](https://imiku.me/2018/10/16/1230.html)
--- index.js
+++ index.js
@@ -4,31 +4,18 @@ const credentials = require('./credentials');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const MD5 = require('js-md5');
+const Parser = require('rss-parser');
 const Agent = require('socks5-https-client/lib/Agent');
 const clc = require("cli-color");
+const dayjs = require('dayjs');
 
-let bot = new CQHttp({
+const bot = new CQHttp({
     apiRoot: 'http://127.0.0.1:5700/',
     accessToken: credentials.accessToken,
     secret: credentials.secret
 });
 
-function htmlDecode(str) {
-    // 一般可以先转换为标准 unicode 格式（有需要就添加：当返回的数据呈现太多\\\u 之类的时）
-    str = unescape(str.replace(/\\u/g, "%u"));
-    // 再对实体符进行转义
-    // 有 x 则表示是16进制，$1 就是匹配是否有 x，$2 就是匹配出的第二个括号捕获到的内容，将 $2 以对应进制表示转换
-    str = str.replace(/&#(x)?(\w+);/g, function ($, $1, $2) {
-        return String.fromCharCode(parseInt($2, $1 ? 16 : 10));
-    });
-    return str;
-}
-
-function log(log){
-    console.log(clc.cyan(new Date() + '：') + log);
-}
-
-async function fanyi(str) { // 百度翻译
+const baiduTranslate = async (str) => {
     const appid = credentials.baidu.appid;
     const key = credentials.baidu.key;
     const salt = (new Date).getTime();
@@ -38,7 +25,7 @@ async function fanyi(str) { // 百度翻译
     const str1 = appid + query + salt + key;
     const sign = MD5(str1);
 
-    let data = await rp.get('http://api.fanyi.baidu.com/api/trans/vip/translate', {
+    const data = await rp.get('http://api.fanyi.baidu.com/api/trans/vip/translate', {
         qs: {
             q: query,
             appid: appid,
@@ -47,16 +34,22 @@ async function fanyi(str) { // 百度翻译
             to: to,
             sign: sign
         },
-        timeout: 1000 * 60
+        json: true
     });
-    return JSON.parse(data);
+    return data.trans_result[0].dst;
 }
 
-let v = new Object(); // 保存rss每次拉取的时间
-let baseURL = 'https://rsshub.app';
-function h(config, timeout) {
+const log = (log) => {
+    let date = dayjs(new Date()).format('YY年M月D日HH:mm:ss');
+    console.log(clc.cyan(date + '：') + log);
+}
+
+let upTime = new Object(); // 保存rss每次拉取的时间
+const baseURL = 'https://rsshub.app';
+
+function grss(config, timeout) {
     if (!timeout) timeout = 1000 * 60 * 5;
-    if (!v[config.name]) timeout = 0;
+    if (!upTime[config.name]) timeout = 0;
     setTimeout(() => {
         rp.get(baseURL + config.url, {
             json: true,
@@ -64,95 +57,118 @@ function h(config, timeout) {
             qs: {
                 limit: 1
             }
-        }).then(async e => {
-            const date_published = (new Date(e.items[0].date_published)).getTime();
-            if (!v[config.name]) { // 如果不存在说明是第一次请求
-                log('首次请求' + config.name);
-                v[config.name] = date_published;
-                h(config);
-                return false;
-            }
-            if (v[config.name] < date_published) { //有更新
-                log('发现更新' + config.name)
-                if (e.items[0].title.search('Re') !== -1) { // 如果是回复类型的推文则不推送
-                    h(config);
+        })
+            .then(async e => {
+                // 解析RSS
+                const parser = new Parser();
+                let feed = await parser.parseString(e);
+
+                const date_published = dayjs(feed.items[0].date_published).unix();
+                if (!upTime[config.name]) { // 如果不存在说明是第一次请求
+                    log('首次请求' + config.name);
+                    upTime[config.name] = date_published;
+                    grss(config);
                     return false;
                 }
-                const $ = cheerio.load('<div class="_x">' + htmlDecode(e.items[0].summary) + '</div>');
-                let imgArr = '';
-                if ($('img').length !== 0){ // 如果有图片，请求并转换为base64编码
-                    let promises = new Array();
-                    $('img').each(function () {
-                        let src = $(this).attr('src');
-                        if (src.indexOf('https') === -1) {
-                            src = src.replace(/http/, 'https');
-                        }
-                        promises.push(rp({
-                            method: 'GET',
-                            url: src,
-                            timeout: 1000 * 60,
-                            agentClass: Agent,
-                            agentOptions: {
-                                socksHost: '127.0.0.1',
-                                socksPort: 1080
-                            },
-                            encoding: null
-                        }))
-                    })
-                    try {
-                        let miao = await Promise.all(promises);
-                        miao.forEach(response => {
-                            const data = "base64://" + Buffer.from(response, 'utf-8').toString('base64');
-                            imgArr += '[CQ:image,file=' + data + ']';
-                        });
-                    } catch (error) {
-                        log(config.name + '：图片抓取失败' + error);
-                        h(config, 1000 * 60 * 1);
+
+                if (upTime[config.name] < date_published) { //有更新
+                    log('发现更新' + config.name)
+
+                    if (feed.items[0].title.search('Re') !== -1) { // 如果是回复类型的推文则不推送
+                        log('回复推文，不推送');
+                        grss(config);
                         return false;
                     }
+
+                    // 解析HTML
+                    const $ = cheerio.load(feed.items[0].content);
+
+                    let imgArr = '';
+
+                    if ($('img').length > 0){ // 如果有图片，请求并转换为base64编码
+                        let promises = new Array();
+                        $('img').each(function () {
+                            const src = $(this).attr('src');
+
+                            // 把http链接转换成https
+                            if(/https?/.test(src)){
+                                src = src.replace(/https?/, 'https');
+                            }
+                            
+                            promises.push(rp({
+                                method: 'GET',
+                                url: src,
+                                timeout: 1000 * 60,
+                                agentClass: Agent,
+                                agentOptions: {
+                                    socksHost: '127.0.0.1',
+                                    socksPort: 1080
+                                },
+                                encoding: null
+                            }))
+
+                        })
+                        try {
+                            let images = await Promise.all(promises);
+                            images.forEach(response => {
+                                const data = "base64://" + Buffer.from(response, 'utf-8').toString('base64');
+                                imgArr += '[CQ:image,file=' + data + ']';
+                            });
+                        } catch (error) {
+                            log(config.name + '：图片抓取失败' + error);
+                            grss(config, 1000 * 60 * 1);
+                            return false;
+                        }
+                    }
+
+
+                    
+                    const message = {
+                        text: '【@' + config.name.split('-')[1] + '】的' + config.name.split('-')[0] + '更新了！',
+                        title: feed.items[0].title === '' ? '' : `标题：${feed.items[0].title}\n`,
+                        content: feed.items[0].contentSnippet,
+                        translateText: (await baiduTranslate(feed.items[0].contentSnippet)),
+                        imgs: imgArr === '' ? '' : ('媒体：\n' + imgArr + '\n'),
+                        url: feed.items[0].link,
+                        date: dayjs(feed.items[0].pubDate).format('YY年M月D日HH:mm:ss')
+                    }
+                    bot('send_group_msg', {
+                        group_id: config.group_id,
+                        message: 
+                            `${message.text}\n` + 
+                            '-------------------------------------------\n' + 
+                            `${message.title}` + 
+                            `内容：${message.content}\n` + 
+                            `${message.translateText}` + 
+                            `${message.imgs}` + 
+                            '-------------------------------------------\n' + 
+                            `原链接：${message.url}\n` + 
+                            `日期：${message.date}`
+                    })
+                        .then(() => {
+                            log(config.name + '更新发送成功');
+                            upTime[config.name] = date_published;
+                            grss(config);
+                        })
+                        .catch(error => {
+                            log(config.name + ' 更新发送失败：' + error);
+                            grss(config, 1000 * 60 * 1);
+                        })
+                } else { //没有更新
+                    log(config.name + ' 没有更新  最后更新于：' + dayjs(feed.items[0].pubDate).format('YY年M月D日HH:mm:ss'));
+                    grss(config);
                 }
-                
-                const text = '【@' + config.name.split('-')[1] + '】的' + config.name.split('-')[0] + '更新了！';
-                const title = e.items[0].title === '' ? '' : ('\n标题：' + e.items[0].title);
-                const content = $('._x').text();
-                const fanyiText = (await fanyi(content)).trans_result[0].dst;
-                const imgs = imgArr === '' ? '' : ('\n媒体：\n' + imgArr);
-                const url = e.items[0].url;
-                bot('send_group_msg', {
-                    group_id: config.group_id,
-                    message: `\
-                        ${text}
-                        -------------------------------------------\
-                        ${title}
-                        内容：${content}
-                        翻译：${fanyiText}\
-                        ${imgs}
-                        -------------------------------------------
-                        原链接：${url}
-                        日期：${e.items[0].date_published}\
-                    `.replace(/^[^\S\n]+/gm, '')
-                }).then(() => {
-                    log(config.name + '更新发送成功');
-                    v[config.name] = date_published;
-                    h(config);
-                }).catch(err => {
-                    log(config.name + ' 更新发送失败：' + err);
-                    h(config, 1000 * 60 * 1);
-                })
-            } else { //没有更新
-                log(config.name + ' 没有更新  最后更新于：' + new Date(e.items[0].date_published));
-                h(config);
-            }
-        }).catch(error => {
-            log(config.name + '请求RSSHub失败' + error);
-            h(config, 1000 * 60 * 1);
-        })
+            })
+            .catch(error => {
+                log(config.name + '请求RSSHub失败' + error);
+                grss(config, 1000 * 60 * 1);
+            })
     }, timeout);
 };
 
-credentials.data.forEach((p, index) => {
+credentials.list.forEach((config, index) => {
     setTimeout(() => {
-        h(p)
+        grss(config)
     }, 1000 * 10 * index);
 })
 
